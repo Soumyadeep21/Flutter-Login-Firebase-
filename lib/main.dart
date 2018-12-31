d3bfeecf73310709fbb0f5c63f481cb37ddca936
Soumyadeep <soumo.sinha21@gmail.com>
2019-01-01 01:19:17 +0530

    Removed unnecessary codes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_login/pages/home_page.dart';
-import 'package:flutter_login/pages/login_page.dart';
-import 'package:flutter_login/pages/registration_page.dart';
 import 'package:flutter_login/auth.dart';
 import 'package:flutter_login/pages/root_page.dart';
 import 'auth.dart';
@@ -23,11 +20,8 @@ class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: <String,WidgetBuilder>{
-        "login" : (BuildContext context) => new LoginPage(auth: widget.auth,),
-        "registration" : (BuildContext context) => new RegistrationPage(auth: widget.auth,),
-        "home" : (BuildContext context) => new HomePage(auth: widget.auth,),
-      },
+      debugShowCheckedModeBanner: false,
+      title: "Login App",
       home: RootPage(auth: widget.auth,),
     );
   }
